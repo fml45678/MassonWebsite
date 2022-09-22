faadba96290acfd266f2da42cba04b8fee389751
Masson LeMieux <fml45678@gmail.com>
2022-09-22 17:12:58 -0400

    changed homepage image to a gif

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div className="homeScreen">
-            <img className="homeScreen__image" src="massonguitar.png" alt="Masson LeMieux holding on stage with guitar" />            
-        </div>
-    )
+  return (
+    <div className="homeScreen">
+      <img
+        className="homeScreen__image"
+        src="masson.gif"
+        alt="Masson LeMieux animation"
+      />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
