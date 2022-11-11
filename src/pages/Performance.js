7db737f92898f1c335ca7a03792e64f5020505c1
fml45678 <84721089+fml45678@users.noreply.github.com>
2022-11-10 23:42:07 -0500

    added a link to guitar lessons and changed bio width for smaller screens

--- src/pages/Performance.js
+++ src/pages/Performance.js
@@ -1,43 +1,51 @@
-import React from 'react';
-import './Performance.css';
+import React from "react";
+import "./Performance.css";
 
 function Performance() {
-    return (
-        <div className='performance'>
-            <div className="performance__flexContainer">
-                <div className="performance__video">
-                    <iframe title="videoOne"
-                        src="https://www.youtube.com/embed/rB8TYrsjjoE">        
-                    </iframe>
-                </div>
-                <div className="performance__video">
-                    <iframe title="videoTwo"
-                        src="https://www.youtube.com/embed/fM70uPsMLis">        
-                    </iframe>
-                </div>
-
-                <div className="performance__video">
-                    <iframe title="videoTwo"
-                        src="https://www.youtube.com/embed/Yt7ikda6SxQ">        
-                    </iframe>
-                </div>
-                
-                <div className="performance__video">
-                    <iframe title="videoTwo"
-                        src="https://www.youtube.com/embed/ZmhMRCNwOLE">        
-                    </iframe>
-                </div>
-                
-                <div className="performance__video">
-                    <iframe title="videoTwo"
-                        src="https://www.youtube.com/embed/yTXoZMfFCMM">        
-                    </iframe>
-                </div>
-                
-                
-            </div>
+  return (
+    <div className="performance">
+      <div className="performance__flexContainer">
+        <h2>Teaching</h2>
+        <a target="blank" href="https://lemieuxstudios.com">
+          click here for guitar lessons
+        </a>
+        <h2>Performance</h2>
+        <div className="performance__video">
+          <iframe
+            title="videoOne"
+            src="https://www.youtube.com/embed/rB8TYrsjjoE"
+          ></iframe>
         </div>
-    )
+        <div className="performance__video">
+          <iframe
+            title="videoTwo"
+            src="https://www.youtube.com/embed/fM70uPsMLis"
+          ></iframe>
+        </div>
+
+        <div className="performance__video">
+          <iframe
+            title="videoTwo"
+            src="https://www.youtube.com/embed/Yt7ikda6SxQ"
+          ></iframe>
+        </div>
+
+        <div className="performance__video">
+          <iframe
+            title="videoTwo"
+            src="https://www.youtube.com/embed/ZmhMRCNwOLE"
+          ></iframe>
+        </div>
+
+        <div className="performance__video">
+          <iframe
+            title="videoTwo"
+            src="https://www.youtube.com/embed/yTXoZMfFCMM"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Performance
+export default Performance;
