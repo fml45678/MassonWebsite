7f1dcc5e4edc45f4edf10af26aa323efd822bc1e
fml45678 <84721089+fml45678@users.noreply.github.com>
2022-11-12 22:21:42 -0500

    filled out some links

--- src/pages/LinkTree.js
+++ src/pages/LinkTree.js
@@ -2,7 +2,38 @@ import React from "react";
 import "./LinkTree.css";
 
 function LinkTree() {
-  return <div>LinkTree</div>;
+  return (
+    <div>
+      <ul className="links">
+        <li>
+          <a href="/">Website</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/channel/UC_Vy5Ay28Op5LbMVINnsR8Q">
+            Youtube
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/fml45678/">Instagram</a>
+        </li>
+        <li>
+          <a href="https://www.tiktok.com/@massonlemieux">Tiktok</a>
+        </li>
+        <li>
+          <a href="https://www.etsy.com/shop/useMidi">Midi Instruments</a>
+        </li>
+        <li>
+          <a href="https://www.etsy.com/shop/LeMieuxStudios">T-shirts</a>
+        </li>
+        <li>
+          <a href="https://lemieuxstudios.com">Guitar Lessons</a>
+        </li>
+        <li>
+          <a href="/podcast">The Masson LeMieux Show: Sports Talk</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default LinkTree;
